86efb88d2e21f3547b76760931d2e801df62b3db
“Ilknur <ilkxeren@gmail.com>
2018-01-01 18:44:56 -0500

    Problem 1

---
+++ project-euler.rb
@@ -0,0 +1,19 @@
+# Problem 1
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+sum = 0
+n = 0
+
+1000.times do
+  if n %3 == 0 || n % 5 == 0
+    sum = sum + n 
+    p sum
+  else
+
+  end
+    n += 1
+end
+
+p sum
